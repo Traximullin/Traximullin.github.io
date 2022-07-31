c163932795a757033ab4b310cc691d572d3c009e
Traximullin <66427615+Traximullin@users.noreply.github.com>
2022-07-31 20:37:01 +0300

    error-handler

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 // Styles
 import '../styles/globals.scss'
+// Next error
+import Error from "next/error";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  if (pageProps.error) {
+    return <Error statusCode={pageProps.error.statusCode} title={pageProps.error.message} />;
+  }
+  return (
+    <Component {...pageProps} />
+  );
 }
 
 export default MyApp
